80a822abe499e1bf4a48e30aaed6ed6fd17a8c1f
Anshu Kumar <95365748+anshuhtwt@users.noreply.github.com>
2023-03-29 06:07:46 +0530

    Update deploy-ingress-controller-and-apply-yaml.sh

--- bash/deploy-ingress-controller-and-apply-yaml.sh
+++ bash/deploy-ingress-controller-and-apply-yaml.sh
@@ -8,10 +8,10 @@ helm upgrade --install ingress-nginx ingress-nginx \
   
   
 # apply yaml files
-kubectl apply -R -f /home/ubuntu/Kubernetes-TODO/frontend/
-kubectl apply -R -f /home/ubuntu/Kubernetes-TODO/frontend/
-kubectl apply -R -f /home/ubuntu/Kubernetes-TODO/log-message-processor/
-kubectl apply -R -f /home/ubuntu/Kubernetes-TODO/todos-api/
-kubectl apply -R -f /home/ubuntu/Kubernetes-TODO/users-api/
-kubectl apply -R -f /home/ubuntu/Kubernetes-TODO/dns-network-policy/
-kubectl apply -R -f /home/ubuntu/Kubernetes-TODO/redis-queue/
+sudo kubectl apply -R -f /home/ubuntu/Kubernetes-TODO/frontend/
+sudo kubectl apply -R -f /home/ubuntu/Kubernetes-TODO/frontend/
+sudo kubectl apply -R -f /home/ubuntu/Kubernetes-TODO/log-message-processor/
+sudo kubectl apply -R -f /home/ubuntu/Kubernetes-TODO/todos-api/
+sudo kubectl apply -R -f /home/ubuntu/Kubernetes-TODO/users-api/
+sudo kubectl apply -R -f /home/ubuntu/Kubernetes-TODO/dns-network-policy/
+sudo kubectl apply -R -f /home/ubuntu/Kubernetes-TODO/redis-queue/
