573e19a7b6e1e14fd0035cd24a055ca484c07fca
Anshu Kumar <95365748+anshuhtwt@users.noreply.github.com>
2023-04-04 02:00:23 +0530

    Update deploy-ingress-controller-and-apply-yaml.sh

--- bash/deploy-ingress-controller-and-apply-yaml.sh
+++ bash/deploy-ingress-controller-and-apply-yaml.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # deploy the ingress controller
-helm upgrade --install ingress-nginx ingress-nginx \
---repo https://kubernetes.github.io/ingress-nginx \
---namespace ingress-nginx --create-namespace
+#helm upgrade --install ingress-nginx ingress-nginx \
+#--repo https://kubernetes.github.io/ingress-nginx \
+#--namespace ingress-nginx --create-namespace
   
   
   
 # apply yaml files
-kubectl apply -R -f /home/cloudshell-user/Kubernetes-TODO/frontend/
-kubectl apply -R -f /home/cloudshell-user/Kubernetes-TODO/frontend/
-kubectl apply -R -f /home/cloudshell-user/Kubernetes-TODO/log-message-processor/
-kubectl apply -R -f /home/cloudshell-user/Kubernetes-TODO/todos-api/
-kubectl apply -R -f /home/cloudshell-user/Kubernetes-TODO/users-api/
-kubectl apply -R -f /home/cloudshell-user/Kubernetes-TODO/dns-network-policy/
-kubectl apply -R -f /home/cloudshell-user/Kubernetes-TODO/redis-queue/
+kubectl apply -R -f /Users/anshukumar/workspace/Kubernetes-TODO/frontend/
+kubectl apply -R -f /Users/anshukumar/workspace/Kubernetes-TODO/frontend/
+kubectl apply -R -f /Users/anshukumar/workspace/Kubernetes-TODO/log-message-processor/
+kubectl apply -R -f /Users/anshukumar/workspace/Kubernetes-TODO/todos-api/
+kubectl apply -R -f /Users/anshukumar/workspace/Kubernetes-TODO/users-api/
+kubectl apply -R -f /Users/anshukumar/workspace/Kubernetes-TODO/dns-network-policy/
+kubectl apply -R -f /Users/anshukumar/workspace/Kubernetes-TODO/redis-queue/
